9a1405fd99898684312956c91c9e14adabca33d5
Rashwan Mohamed <rashwan.mohamed@scandiweb.com>
2025-10-14 14:53:50 +0300

    assingedToBugFixed

--- app/code/EDU/SupportTickets/Controller/Adminhtml/Ticket/Delete.php
+++ app/code/EDU/SupportTickets/Controller/Adminhtml/Ticket/Delete.php
@@ -4,7 +4,6 @@ namespace EDU\SupportTickets\Controller\Adminhtml\Ticket;
 
 use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
-use Magento\Framework\View\Result\PageFactory;
 use EDU\SupportTickets\Api\TicketRepositoryInterface;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Exception\CouldNotDeleteException;
@@ -14,14 +13,11 @@ class Delete extends Action
     const ADMIN_RESOURCE = 'EDU_SupportTickets::tickets';
     
     protected $ticketRepository;
-    protected $resultPageFactory;
 
     public function __construct(
         Context $context,
-        PageFactory $resultPageFactory,
         TicketRepositoryInterface $ticketRepository
     ) {
-        $this->resultPageFactory = $resultPageFactory;
         $this->ticketRepository = $ticketRepository;
         parent::__construct($context);
     }
@@ -30,18 +26,10 @@ class Delete extends Action
     {
         $resultRedirect = $this->resultRedirectFactory->create();
         $ticketId = $this->getRequest()->getParam('id');
-        $confirm = $this->getRequest()->getParam('confirm');
         
         if (!$ticketId) {
             $this->messageManager->addErrorMessage(__('Ticket ID is required.'));
             return $resultRedirect->setPath('supporttickets/ticket/index');
-        }
-
-        // If not confirmed, show confirmation page
-        if (!$confirm) {
-            $resultPage = $this->resultPageFactory->create();
-            $resultPage->getConfig()->getTitle()->prepend(__('Delete Ticket Confirmation'));
-            return $resultPage;
         }
         
         try {
