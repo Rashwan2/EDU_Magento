51da52cbef6248c1d4a330cfd65e1c739a782e7a
Rashwan Mohamed <rashwan.mohamed@scandiweb.com>
2025-10-21 13:38:37 +0300

    fixed account space

---
+++ app/code/EDU/SupportTickets/Controller/Account/Tickets.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace EDU\SupportTickets\Controller\Account;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Customer\Model\Session as CustomerSession;
+use EDU\SupportTickets\Api\TicketRepositoryInterface;
+
+class Tickets implements HttpGetActionInterface
+{
+    /**
+     * @var PageFactory
+     */
+    protected $pageFactory;
+    
+    /**
+     * @var CustomerSession
+     */
+    protected $customerSession;
+    
+    /**
+     * @var TicketRepositoryInterface
+     */
+    protected $ticketRepository;
+
+    public function __construct(
+        PageFactory $pageFactory,
+        CustomerSession $customerSession,
+        TicketRepositoryInterface $ticketRepository
+    ) {
+        $this->pageFactory = $pageFactory;
+        $this->customerSession = $customerSession;
+        $this->ticketRepository = $ticketRepository;
+    }
+
+    public function execute()
+    {
+        // Check if customer is logged in
+        if (!$this->customerSession->isLoggedIn()) {
+            $this->customerSession->setAfterAuthUrl('supporttickets/account/tickets');
+            $this->customerSession->authenticate();
+            return;
+        }
+
+        $resultPage = $this->pageFactory->create();
+        $resultPage->getConfig()->getTitle()->set(__('My Support Tickets'));
+
+        // Get customer tickets
+        $customer = $this->customerSession->getCustomer();
+        $customerEmail = $customer->getEmail();
+        $tickets = $this->ticketRepository->getByCustomerEmail($customerEmail);
+        
+        // Pass tickets to the block
+        $resultPage->getLayout()->getBlock('ticket.list')->setData('tickets', $tickets);
+
+        return $resultPage;
+    }
+}
